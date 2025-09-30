00148335761f8717eec41c13ede3d8f204249e23
Hollypie <hbriggs@byupathway.edu>
2025-09-30 13:07:49 -0600

    still trying to fix api docs

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongodb = require('./data/database');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 // Middleware
-app.use(express.json()); // built-in body parser
+app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -17,9 +18,19 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/', require('./routes'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Global error handler for uncaught exceptions
+// Swagger UI
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      url: 'https://cse341-boardgameapi.onrender.com/swagger.json', // deployed JSON
+    },
+  })
+);
+
+// Global error handler
 process.on('uncaughtException', (err, origin) => {
   console.error(`Caught exception: ${err}\nException origin: ${origin}`);
 });
