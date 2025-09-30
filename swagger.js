e96b76de59f4835444e2eb56e736a2792557129b
Hollypie <hbriggs@byupathway.edu>
2025-09-30 09:18:56 -0600

    fixed the API docs so that swagger.json shows host as the render url and not the localhost:3000

--- swagger.js
+++ swagger.js
@@ -3,13 +3,33 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Board Game API',
-    description: 'CSE 341 Weeks 3 and 4 assignment',
+    description: 'CSE 341 Weeks 3 and 4 assignment'
   },
-  host: process.env.HOST || 'localhost:8080',
-  schemes: process.env.SCHEMES ? process.env.SCHEMES.split(',') : ['http'],
+  host: 'cse341-boardgameapi.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
+
+
+
+// const swaggerAutogen = require('swagger-autogen')();
+
+// const doc = {
+//   info: {
+//     title: 'Board Game API',
+//     description: 'CSE 341 Weeks 3 and 4 assignment',
+//   },
+//   host: process.env.HOST || 'localhost:8080',
+//   schemes: process.env.SCHEMES ? process.env.SCHEMES.split(',') : ['http'],
+// };
+
+// const outputFile = './swagger.json';
+// const endpointsFiles = ['./routes/index.js'];
+
+// swaggerAutogen(outputFile, endpointsFiles, doc);
+
+
