e3236a4d05417ccbf4544e5dad783d4529d6f2da
Hollypie <hbriggs@byupathway.edu>
2025-09-30 12:49:46 -0600

    still troubleshooting the api docs

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Board Game API',
     description: 'CSE 341 Weeks 3 and 4 assignment'
   },
-  host: '',
-  schemes: ['']
+  host: 'cse341-boardgameapi.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
