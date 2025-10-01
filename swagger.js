991daeea727f74c4839cc52a527729d68e8b69d5
Hollypie <hbriggs@byupathway.edu>
2025-10-01 07:49:12 -0600

    still trying to fix the swagger api docs

--- swagger.js
+++ swagger.js
@@ -3,18 +3,23 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Board Game API',
-    description: 'CSE 341 Weeks 3 and 4 assignment'
+    description: 'CSE 341 Weeks 3 and 4 assignment',
+    version: '1.0.0'
   },
   host: 'cse341-boardgameapi.onrender.com',
+  basePath: '/',
   schemes: ['https']
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
-
-
+// Generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  console.log('Swagger file generated successfully!');
+  console.log('Host:', doc.host);
+  console.log('Schemes:', doc.schemes);
+});
 
 // const swaggerAutogen = require('swagger-autogen')();
 
