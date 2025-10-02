e108d58a8d9142bb114549cac1a89596e4a6b530
Hollypie <hbriggs@byupathway.edu>
2025-10-02 15:49:19 -0600

    still working on swagger

--- routes/games.js
+++ routes/games.js
@@ -4,18 +4,33 @@ const gamesController = require('../controllers/games');
 const { createGameRules, updateGameRules, gameIdParamRules } = require('../middlewear/games.js');
 const { validate } = require('../middlewear/validator.js');
 
-// GET all games
+/**
+ * GET all games
+ */
 // #swagger.tags = ['Games']
 // #swagger.description = 'Get all games.'
+// #swagger.responses[200] = {
+//     description: 'Array of games returned successfully',
+//     schema: [{ $ref: "#/definitions/Game" }]
+// }
+// #swagger.responses[500] = { description: 'Internal server error' }
 router.get('/', gamesController.getAllGames);
 
-// GET single game by ID
+/**
+ * GET a single game by ID
+ */
 // #swagger.tags = ['Games']
 // #swagger.description = 'Get a single game by its ID.'
 // #swagger.parameters['id'] = { description: 'Game ID', required: true, type: 'string' }
+// #swagger.responses[200] = { description: 'Game found', schema: { $ref: "#/definitions/Game" } }
+// #swagger.responses[404] = { description: 'Game not found' }
+// #swagger.responses[400] = { description: 'Invalid ID format' }
+// #swagger.responses[500] = { description: 'Internal server error' }
 router.get('/:id', gameIdParamRules(), validate, gamesController.getSingleGame);
 
-// POST create a new game
+/**
+ * POST create a new game
+ */
 // #swagger.tags = ['Games']
 // #swagger.description = 'Create a new game.'
 // #swagger.parameters['body'] = {
@@ -24,28 +39,39 @@ router.get('/:id', gameIdParamRules(), validate, gamesController.getSingleGame);
 //     required: true,
 //     schema: { $ref: "#/definitions/Game" }
 // }
-// #swagger.responses[201] = {
-//     description: 'Game created successfully',
-//     schema: { $ref: "#/definitions/Game" }
-// }
+// #swagger.responses[201] = { description: 'Game created successfully', schema: { $ref: "#/definitions/Game" } }
+// #swagger.responses[400] = { description: 'Missing required fields' }
+// #swagger.responses[500] = { description: 'Internal server error' }
 router.post('/', createGameRules(), validate, gamesController.createGame);
 
-// PUT update a game
+/**
+ * PUT update a game by ID
+ */
 // #swagger.tags = ['Games']
 // #swagger.description = 'Update an existing game.'
 // #swagger.parameters['id'] = { description: 'Game ID', required: true, type: 'string' }
 // #swagger.parameters['body'] = {
 //     in: 'body',
-//     description: 'Updated game info',
+//     description: 'Updated game information',
 //     required: true,
 //     schema: { $ref: "#/definitions/Game" }
 // }
+// #swagger.responses[200] = { description: 'Game updated successfully', schema: { $ref: "#/definitions/Game" } }
+// #swagger.responses[400] = { description: 'Invalid ID or missing fields' }
+// #swagger.responses[404] = { description: 'Game not found' }
+// #swagger.responses[500] = { description: 'Internal server error' }
 router.put('/:id', gameIdParamRules(), updateGameRules(), validate, gamesController.updateGame);
 
-// DELETE a game
+/**
+ * DELETE a game by ID
+ */
 // #swagger.tags = ['Games']
-// #swagger.description = 'Delete a game by ID.'
+// #swagger.description = 'Delete a game by its ID.'
 // #swagger.parameters['id'] = { description: 'Game ID', required: true, type: 'string' }
+// #swagger.responses[200] = { description: 'Game deleted successfully' }
+// #swagger.responses[404] = { description: 'Game not found' }
+// #swagger.responses[400] = { description: 'Invalid ID format' }
+// #swagger.responses[500] = { description: 'Internal server error' }
 router.delete('/:id', gameIdParamRules(), validate, gamesController.deleteGame);
 
 module.exports = router;
